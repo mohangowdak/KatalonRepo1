cee90480d1d37dd74ff52598d6b10ae248adfec6
mohangowdak <mohan.k@testyantra.com>
2023-10-04 12:40:52 +0530

    suite 1 uploaded

---
+++ Test Suites/Suite 1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7a893769-ceff-4ca9-8564-68ba66ef9bc0</testSuiteGuid>
+   <testCaseLink>
+      <guid>6eaf0c5c-9bc1-4e72-b6c5-4ce3f9c27b0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/fireflink</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
